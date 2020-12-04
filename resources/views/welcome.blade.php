37826b77daf5fa5062f2f2fdd66bb436d74cd7be
Mursal Rasa <mursal.rasa@auaf.edu.af>
2020-12-04 06:09:57 +0430

    A new line added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <title>Laravel</title>
-
-    </head>
-    <body class="bg-gray-300 container mx-auto py-20">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Laravel</title>
+    
+</head>
+<body class="bg-gray-300 container mx-auto py-20">
     <h2>Welcome to Laravel 8 Course!</h2>
-    </body>
+    <h3>This is a demo course!!</h3>
+</body>
 </html>
